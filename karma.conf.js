bd98cf4127213135b4d3c2264680afbee895b328
Tomas Bagdonas <tomas.bagdonas@registrucentras.lt>
2026-01-28 12:02:16 +0200

    SAV-4805 add Karma configuration and testing setup for Angular components

---
+++ karma.conf.js
@@ -0,0 +1,53 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('karma-junit-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/rc-ses-angular-components'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcov' },
+        { type: 'cobertura' }
+      ]
+    },
+    junitReporter: {
+      outputDir: require('path').join(__dirname, './coverage/rc-ses-angular-components'),
+      outputFile: 'junit.xml',
+      useBrowserName: false
+    },
+    reporters: ['progress', 'kjhtml', 'coverage', 'junit'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    restartOnFileChange: true
+  });
+};
